5aab4f3623e046dd8d359ae712d5e38e1399b70a
Andrey Fedorov <oran9e.red@gmail.com>
2026-02-24 18:08:07 +0100

    Remove test remains

--- Tests/SMAppServiceTest/protocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-@objc protocol HelperProtocol {
-    /// Send a signal to a process (helper runs as root, can signal any process)
-    func sendSignal(_ sig: Int32, toProcessID pid: Int32, withReply reply: @escaping (Bool, String) -> Void)
-
-    /// Check if a process is alive
-    func isProcessAlive(_ pid: Int32, withReply reply: @escaping (Bool) -> Void)
-}
